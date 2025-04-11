acda0759be886b0fe3655b6582b06ccbc67794be
xXAriserXx <j.r.precilla365@gmail.com>
2025-04-11 11:08:06 +0200

    Implemented many things will go back to this version if things go badly, I will now practice with the Suspense tag

--- app/query/route.ts
+++ app/query/route.ts
@@ -1,6 +1,6 @@
  import postgres from 'postgres';
 
- const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'allow' });
 
  async function listInvoices() {
 	const data = await sql`
